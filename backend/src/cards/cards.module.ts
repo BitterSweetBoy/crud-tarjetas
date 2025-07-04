a16fed1e230060ec5e3d40f73b0de459dcead37a
Denis Duerñas <mejiadani113@gmail.com>
2025-07-03 18:13:27 -0600

    fix: missing providers

--- backend/src/cards/cards.module.ts
+++ backend/src/cards/cards.module.ts
@@ -3,10 +3,12 @@ import { DatabaseService } from '../database/database.service';
 import { CardRepository } from '../repositories/card-repository';
 import { CardService } from './card.service';
 import { CardController } from './card.controller';
+import { LogService } from '../services/log.service';
+import { LogRepository } from 'src/repositories';
 
 @Module({
   controllers: [CardController],
-  providers: [DatabaseService, CardRepository, CardService],
+  providers: [DatabaseService, CardRepository, CardService, LogService, LogRepository],
   exports: [CardRepository, CardService],
 })
 export class CardsModule {}

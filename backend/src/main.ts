679c5451a3b4a75d8e0a986f7f3403dff8b1cee9
Denis Duerñas <mejiadani113@gmail.com>
2025-07-04 21:10:47 -0600

    feat: add cors and include frontend service in docker compose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as path from 'path';
+import { config } from 'dotenv';
+
+
+//descomentar para dev
+// config({
+//   path: path.resolve(__dirname, '../../.env'),
+// });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Cards Api')
@@ -21,8 +37,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
-  app.setGlobalPrefix('api');
 
   await app.listen(process.env.PORT ?? 3000);
 }
